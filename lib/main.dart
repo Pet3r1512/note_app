878520fff31401b9399559052e2fab529ac2e2ca
lekhoi2709 <ledinhkhoimd@gmail.com>
2023-05-08 12:05:33 +0700

    setup sign in & up page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:notexpert_mongo/screen/home.dart';
-import 'auth.dart';
+import 'package:notexpert_mongo/auth/sign_in.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SignUp(),
+      home: SignIn(),
     );
   }
 }
---
+++ lib/widgets/reusable_widgets.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:notexpert_mongo/style/app_style.dart';
+
+TextField reusableTextField(
+    String text, IconData icon, bool isPass, TextEditingController controller) {
+  return TextField(
+    controller: controller,
+    obscureText: isPass,
+    enableSuggestions: !isPass,
+    autocorrect: !isPass,
+    cursorColor: Colors.red,
+    style: TextStyle(color: Colors.black.withOpacity(0.9)),
+    decoration: InputDecoration(
+      prefixIcon: Icon(icon, color: AppStyle.pink),
+      labelText: text,
+      filled: true,
+      floatingLabelBehavior: FloatingLabelBehavior.never,
+      fillColor: AppStyle.pink.withOpacity(0.3),
+      border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          borderSide: const BorderSide(width: 0, style: BorderStyle.none)),
+    ),
+    keyboardType:
+        isPass ? TextInputType.visiblePassword : TextInputType.emailAddress,
+  );
+}
